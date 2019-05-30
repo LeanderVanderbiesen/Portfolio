71f5880b0809db7c973f09fa8985297ecca8c6e7
leander vanderbiesen <LeanderVanderbiesen@users.noreply.github.com>
2019-05-30 22:39:35 +0200

    functions

--- wp-content/themes/enfold-child/functions.php
+++ wp-content/themes/enfold-child/functions.php
@@ -1,54 +1,4 @@
 <?php 
-/* FEATURED BLOGSPOTS */
-
-//checkbox
-
- /*function sm_custom_meta() {
-    add_meta_box( 'sm_meta', __( 'Featured Posts', 'sm-textdomain' ), 'sm_meta_callback', 'post' );
-}
-function sm_meta_callback( $post ) {
-    $featured = get_post_meta( $post->ID );
-    ?>
- 
-	<p>
-    <div class="sm-row-content">
-        <label for="meta-checkbox">
-            <input type="checkbox" name="meta-checkbox" id="meta-checkbox" value="yes" <?php if ( isset ( $featured['meta-checkbox'] ) ) checked( $featured['meta-checkbox'][0], 'yes' ); ?> />
-            <?php _e( 'Featured this post', 'sm-textdomain' )?>
-        </label>
-        
-    </div>
-</p>
- 
-    <?php
-}
-add_action( 'add_meta_boxes', 'sm_custom_meta' );
-
-//save meta data input
-
-function sm_meta_save( $post_id ) {
- 
-    // Checks save status
-    $is_autosave = wp_is_post_autosave( $post_id );
-    $is_revision = wp_is_post_revision( $post_id );
-    $is_valid_nonce = ( isset( $_POST[ 'sm_nonce' ] ) && wp_verify_nonce( $_POST[ 'sm_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
- 
-    // Exits script depending on save status
-    if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
-        return;
-    }
- 
- // Checks for input and saves
-if( isset( $_POST[ 'meta-checkbox' ] ) ) {
-    update_post_meta( $post_id, 'meta-checkbox', 'yes' );
-} else {
-    update_post_meta( $post_id, 'meta-checkbox', '' );
-}
- 
-}
-add_action( 'save_post', 'sm_meta_save' );*/
-
-
 
 //EMPLOYMENT WIDGET
 
@@ -149,11 +99,11 @@ class My_Employment_Widget extends WP_Widget {
     }
     add_action( 'widgets_init', 'load_visits' );
 
-    /*add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+    add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
  
     function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-}*/
+}
 
  ?>
 
